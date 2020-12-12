489f0344aac5699ef92364f9825a895537e0d4e3
KentarouTawara <sironagasuken2@gmail.com>
2020-12-12 15:26:14 +0900

    add bootstrap and gmd

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,13 +2,12 @@ const { environment } = require('@rails/webpacker')
 
 // jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
-  })
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        Popper: ['popper.js', 'default']
+    })
 )
 
 module.exports = environment

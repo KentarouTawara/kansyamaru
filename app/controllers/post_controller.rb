98c3d83f8f4daa0391115a94d8bb6eeb801ec7d7
KentarouTawara <sironagasuken2@gmail.com>
2020-12-19 19:10:24 +0900

    arrange display

--- app/controllers/post_controller.rb
+++
@@ -1,8 +0,0 @@
-class PostController < ApplicationController
-  def new
-    @post = Post.new
-  end
-
-  def create
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'post#new'
+  root to: 'posts#new'
   resources :posts
 end

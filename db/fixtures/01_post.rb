c390be5d91d28a5b019938bac9e26294fb9dff02
KentarouTawara <sironagasuken2@gmail.com>
2020-12-20 22:46:59 +0900

    add seed fu

---
+++ db/fixtures/01_post.rb
@@ -0,0 +1,65 @@
+Post.seed do |s|
+  s.id = 1
+  s.name = 'たなか'
+  s.description = "外は寒いからもう少し厚い服を着なさいよ、と母は気遣ってくれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 2
+  s.name = 'よしだ'
+  s.description = "公園で遊んでいる家族を見るとほっこりした気分になれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 3
+  s.name = 'よしまさ'
+  s.description = "スーパーのレジ打ちをするバイトの子は今日も丁寧にお釣りを渡してくれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 4
+  s.name = 'MAX'
+  s.description = "RUNTEQの講師の方は答えだけでなく考え方も教えてくれる。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 5
+  s.name = 'おばあちゃん'
+  s.description = "今日は墓参りに来てくれてありがとう。風邪引かないようにね。"
+end
+
+Post.seed do |s|
+  s.id = 6
+  s.name = 'Hashimoto'
+  s.description = "買ったお店じゃないのに急に取れた眼鏡のネジを直してくれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 7
+  s.name = '沢田'
+  s.description = "上司はミスをかばって守ってくれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 8
+  s.name = '立川'
+  s.description = "カフェの店員さんは最寄りのバス停を教えてくれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 9
+  s.name = 'ボンバーマン'
+  s.description = "落ち込んだ私の背中を後ろからギュッとしてくれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 10
+  s.name = '路傍の石'
+  s.description = "母はジャンプを買ってきてくれた。ありがとう。"
+end
+
+Post.seed do |s|
+  s.id = 11
+  s.name = '夕陽'
+  s.description = "公園は陽が照っていて気持ちよかった。お天道様、ありがとう。"
+end

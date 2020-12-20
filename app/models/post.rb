2bc4ebb23577640ebf03fed54c0a747d77e66504
KentarouTawara <sironagasuken2@gmail.com>
2020-12-20 16:35:17 +0900

    add thank you validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :name, presence: true, length: { maximum: 75 }
-  validates :description, presence: true, length: { maximum: 300 }
+  validates :description, presence: true, 
+    length: { maximum: 300 },
+    format: { with: /\A(?=.*ありがとう).*\z/, message: "に「ありがとう」の言葉がほしいです" }
 end
